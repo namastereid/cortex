42d0f9fe53b4623e7b9e322e0b35498fa77ef7e1
namastereid <namaste.reid@gmail.com>
2016-01-28 15:12:08 +0000

    added cors support using plugin, does not work for POST :(

--- node-api/lib/index.js
+++ node-api/lib/index.js
@@ -5,11 +5,19 @@
 module.exports.getComment = function(event, cb) {
 
   var response = {
-    message: [
+    comments: [
       {id: 1, author: "Peter Hunt", text: "This is one comment"},
       {id: 2, author: "Jordan Walke", text: "This is *another* comment"} 
     ]
   };
+
+  return cb(null, response);
+};
+
+module.exports.postComment = function(event, cb) {
+
+  console.log(event);
+  var response = "success!";
 
   return cb(null, response);
 };
